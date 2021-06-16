d9dba938920e8f6a5b242fd645739040ea32ac2e
chad svastisalee <swasgrafx@gmail.com>
2021-06-16 16:05:17 -0500

    Completed Users CRUD

---
+++ public/users/index.php
@@ -0,0 +1,17 @@
+<?php
+require '../../config/keys.php';
+require '../../core/db_connect.php';
+
+$meta=[];
+$meta['title']="MicroTrain's Blog";
+
+$content="<h1>MicroTrains Users</h1>";
+$stmt = $pdo->query('SELECT * FROM users');
+
+while($row = $stmt->fetch()){
+  $content .= "<div><a href=\"view.php?id={$row['id']}\">{$row['first_name']}{$row['last_name']}</a></div>";
+}
+
+$content .= "<br><hr><div><a href=\"add.php\">New User</a></div><br>";
+
+require '../../core/layout.php';
